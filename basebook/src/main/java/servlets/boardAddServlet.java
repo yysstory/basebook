2f2076ab7887b55570d43320e48608a7713c7e1b
yun <yun@yun-PC>
2014-11-22 20:35:47 +0900

    help..

--- basebook/src/main/java/servlets/boardAddServlet.java
+++ basebook/src/main/java/servlets/boardAddServlet.java
@@ -7,26 +7,17 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-/**
- * Servlet implementation class boardAddServlet
- */
-@WebServlet("/boardAddServlet")
+
+@WebServlet("/add")
 public class boardAddServlet extends GenericServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see GenericServlet#GenericServlet()
-     */
-    public boardAddServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see Servlet#service(ServletRequest request, ServletResponse response)
-	 */
+
+	
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		System.out.println("add");
+		
 	}
 
 }
--- basebook/src/main/java/servlets/dao/BoardDao.java
+++ basebook/src/main/java/servlets/dao/BoardDao.java
@@ -1,5 +1,112 @@
 package servlets.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import servlets.domain.Board;
+
+@Component
 public class BoardDao {
 
+	  @Autowired
+	  SqlSessionFactory sqlSessionFactory;
+
+	  public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+		    this.sqlSessionFactory = sqlSessionFactory;
+		  }
+
+	 public BoardDao() {}
+
+
+	  
+	  public void insert(Board board) {
+		    SqlSession sqlSession = sqlSessionFactory.openSession();
+		    try {
+		      sqlSession.insert(
+		        "servlets.dao.BoardDao.insert", board);
+		      sqlSession.commit();
+		    } finally {
+		      sqlSession.close();
+		    }
+		  }
+	 
 }
+
+/*@Component
+public class ProductDao {
+  @Autowired
+  SqlSessionFactory sqlSessionFactory;
+  
+  public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+    this.sqlSessionFactory = sqlSessionFactory;
+  }
+
+  public ProductDao() {}
+
+  public Product selectOne(int no) {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    
+    try {
+      return sqlSession.selectOne(
+        "java02.test19.server.ProductDao.selectOne", 
+        no  new Integer(no) );
+    } finally {
+      sqlSession.close();
+    }
+  }
+  
+  public void update(Product product) {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      sqlSession.update(
+        "java02.test19.server.ProductDao.update", product);
+      sqlSession.commit();
+    } finally {
+      sqlSession.close();
+    }
+  }
+  
+  public void delete(int no) {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      sqlSession.delete(
+        "java02.test19.server.ProductDao.delete", no);
+      sqlSession.commit();
+    } finally {
+      sqlSession.close();
+    }
+  }
+  
+  public List<Product> selectList(int pageNo, int pageSize) {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    
+    HashMap<String,Object> paramMap = new HashMap<>();
+    paramMap.put("startIndex", ((pageNo - 1) * pageSize));
+    paramMap.put("pageSize", pageSize);
+    
+    try {
+      return sqlSession.selectList(
+        // 네임스페이스 + SQL문 아이디
+        "java02.test19.server.ProductDao.selectList", 
+        paramMap  SQL문을 실행할 때 필요한 값 전달 );
+    } finally {
+      sqlSession.close();
+    }
+  }
+  
+  public void insert(Product product) {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      sqlSession.insert(
+        "java02.test19.server.ProductDao.insert", product);
+      sqlSession.commit();
+    } finally {
+      sqlSession.close();
+    }
+  }
+}
+
+
+*/
